7fecc9e5a201388a24a1f95af3af4bc585ed392d
brunorrp13 <brunorrp13@hotmail.com>
2021-06-29 22:59:27 -0300

    Estrutura Inicial

---
+++ app/src/main/java/com/example/weatherapp/domain/usecase/GetWeatherForecastUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.weatherapp.domain.usecase
+
+import com.example.weatherapp.domain.repository.WeatherForecastRepository
+
+class GetWeatherForecastUseCase(private val weatherForecastRepository: WeatherForecastRepository) {
+
+
+
+}
